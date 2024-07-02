7397f5717550d77be4f886df9e6658679c463969
FacuMozo <facumozo98@gmail.com>
2024-07-02 14:30:06 -0300

    inital commit

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -9,7 +9,8 @@ const MovieCard = ({movie: movie}) => {
           </div>
           <div >
             <img src={movie.Poster !== 'N/A' ? movie.Poster : "https://via.placeholder.com/400"} alt={movie.Title} />
-          </div>
+        </div>
+        
           <div>
             <span>{movie.Type}</span>
             <h3>{movie.Title} </h3>
